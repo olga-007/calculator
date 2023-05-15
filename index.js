204bfde3efaea966b3da3e20807b4ed96e99d2dd
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-05-15 00:32:57 -0700

    Implement web UI

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // see https://www.theodinproject.com/lessons/foundations-calculator
+
+function addElement(type, parent, cssClass) {
+    const element = document.createElement(type);
+    if (cssClass) {
+        element.classList.add(cssClass);
+    }
+    parent.appendChild(element);
+    return element;
+}
+
+function addDiv(parent, cssClass) {
+    return addElement('div', parent, cssClass);
+}
+
+function addButton(parent, text, cssClass) {
+    const button = addElement('button', parent, cssClass);
+    button.innerText = text;
+    return button;
+}
+
+const container = document.getElementById('container');
+
+const display = addDiv(container, 'display');
+display.textContent = '1234567890';
+
+const buttonBox = addDiv(container, 'button-box');
+
+const digitSection = addDiv(buttonBox, 'digit-box');
+for (let i = 2; i >= 0; i--) {
+    const row = addDiv(digitSection, 'digit-row');
+    for (let j = 0; j < 3; j++) {
+        addButton(row, i * 3 + j + 1);
+    }
+}
+const row = addDiv(digitSection, 'digit-row');
+addButton(row, 0, 'btn-zero');
+const dotBtn = addButton(row, '.');
+dotBtn.disabled = true;
+
+const operationSection = addDiv(buttonBox);
+['/', '*', '-', '+'].forEach((operation) => {
+    addButton(operationSection, operation);
+});
+
+const commandSection = addDiv(buttonBox);
+const bkspBtn = addButton(commandSection, '\u2190');
+bkspBtn.disabled = true;
+const clearBtn = addButton(commandSection, 'C');
+clearBtn.disabled = true;
+addButton(commandSection, '=', 'btn-equals');
