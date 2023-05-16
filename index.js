153da9ae109660318f82bd348f771d62c18f1538
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-05-15 20:29:05 -0700

    Implement arithmetic operations and equals

--- index.js
+++ index.js
@@ -1,22 +1,98 @@
 // see https://www.theodinproject.com/lessons/foundations-calculator
 
 const MAX_TOTAL_LENGTH = 27;
+const TIMES = '\u00D7';
+const BACKSPACE = '\u2190';
+const OPERATIONS = ['/', TIMES, '-', '+'];
 
 let display;
+let num1;
+let num2;
+let currentOp;
+let isFinalCalculated;
+let isError;
+
+function updateDisplay() {
+    display.textContent = num1 + currentOp + num2;
+}
 
 function initDisplay() {
-    display.textContent = '0';
+    num1 = '';
+    num2 = '0';
+    currentOp = '';
+    isFinalCalculated = false;
+    isError = false;
+    updateDisplay();
+}
+
+function add(a, b) {
+    return +a + +b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (+b === 0) {
+        return 'Dividing by zero, are we?';
+    }
+    return a / b;
+}
+
+const opFunctions = {
+    '/': divide,
+    [TIMES]: multiply,
+    '-': subtract,
+    '+': add,
+};
+
+function applyOperation() {
+    const result = opFunctions[currentOp](num1, num2 === '' ? num1 : num2);
+    if (Number.isNaN(result)) {
+        isError = true;
+    }
+    return result;
+}
+
+function calculateFinal() {
+    if (currentOp) {
+        num2 = applyOperation();
+        num1 = '';
+        currentOp = '';
+        updateDisplay();
+    }
+    isFinalCalculated = true;
+}
+
+function printOperation() {
+    if (!isError && display.textContent.length < MAX_TOTAL_LENGTH) {
+        if (!OPERATIONS.includes(display.textContent.at(-1))) {
+            num1 = currentOp ? applyOperation() : num2;
+            num2 = '';
+        }
+        currentOp = this.innerText;
+        updateDisplay();
+    }
 }
 
 function printDigit() {
-    if (display.textContent.length < MAX_TOTAL_LENGTH) {
-        if (display.textContent === '0') {
+    if (!isError && display.textContent.length < MAX_TOTAL_LENGTH) {
+        if (isFinalCalculated) {
+            num2 = this.innerText; // overwrite
+            isFinalCalculated = false;
+        } else if (num2 === '0') {
             if (this.innerText !== '0') {
-                display.textContent = this.innerText;
+                num2 = this.innerText;
             }
         } else {
-            display.textContent += this.innerText;
+            num2 += this.innerText;
         }
+        updateDisplay();
     }
 }
 
@@ -62,20 +138,20 @@ dotBtn.disabled = true;
 
 const operationSection = addDiv(buttonBox);
 
-['/', '*', '-', '+'].forEach((operation) => {
+OPERATIONS.forEach((operation) => {
     const btn = addButton(operationSection, operation);
-    btn.disabled = true;
+    btn.addEventListener('click', printOperation);
 });
 
 const commandSection = addDiv(buttonBox);
 
-const bkspBtn = addButton(commandSection, '\u2190', 'btn-bksp');
+const bkspBtn = addButton(commandSection, BACKSPACE, 'btn-bksp');
 bkspBtn.disabled = true;
 
 const clearBtn = addButton(commandSection, 'C', 'btn-clear');
 clearBtn.addEventListener('click', initDisplay);
 
 const equalsBtn = addButton(commandSection, '=', 'btn-equals');
-equalsBtn.disabled = true;
+equalsBtn.addEventListener('click', calculateFinal);
 
 initDisplay();
