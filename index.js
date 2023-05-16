2afb825bed347acab03fb64425f837987644fd88
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-05-15 17:39:16 -0700

    Implement digits and clear button

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // see https://www.theodinproject.com/lessons/foundations-calculator
 
+const MAX_TOTAL_LENGTH = 27;
+
+let display;
+
+function initDisplay() {
+    display.textContent = '0';
+}
+
+function printDigit() {
+    if (display.textContent.length < MAX_TOTAL_LENGTH) {
+        if (display.textContent === '0') {
+            if (this.innerText !== '0') {
+                display.textContent = this.innerText;
+            }
+        } else {
+            display.textContent += this.innerText;
+        }
+    }
+}
+
 function addElement(type, parent, cssClass) {
     const element = document.createElement(type);
     if (cssClass) {
@@ -20,33 +40,42 @@ function addButton(parent, text, cssClass) {
 }
 
 const container = document.getElementById('container');
-
-const display = addDiv(container, 'display');
-display.textContent = '1234567890';
+display = addDiv(container, 'display');
 
 const buttonBox = addDiv(container, 'button-box');
 
 const digitSection = addDiv(buttonBox, 'digit-box');
+
 for (let i = 2; i >= 0; i--) {
     const row = addDiv(digitSection, 'digit-row');
     for (let j = 0; j < 3; j++) {
-        addButton(row, i * 3 + j + 1);
+        const btn = addButton(row, i * 3 + j + 1);
+        btn.addEventListener('click', printDigit);
     }
 }
 const row = addDiv(digitSection, 'digit-row');
-addButton(row, 0, 'btn-zero');
+const zeroBtn = addButton(row, 0, 'btn-zero');
+zeroBtn.addEventListener('click', printDigit);
+
 const dotBtn = addButton(row, '.');
 dotBtn.disabled = true;
 
 const operationSection = addDiv(buttonBox);
+
 ['/', '*', '-', '+'].forEach((operation) => {
     const btn = addButton(operationSection, operation);
     btn.disabled = true;
 });
 
 const commandSection = addDiv(buttonBox);
+
 const bkspBtn = addButton(commandSection, '\u2190', 'btn-bksp');
 bkspBtn.disabled = true;
+
 const clearBtn = addButton(commandSection, 'C', 'btn-clear');
+clearBtn.addEventListener('click', initDisplay);
+
 const equalsBtn = addButton(commandSection, '=', 'btn-equals');
 equalsBtn.disabled = true;
+
+initDisplay();
